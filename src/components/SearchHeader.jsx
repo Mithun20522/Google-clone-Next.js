e157ee50b29c4582434438074288f9ec4df32c95
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-01 02:51:33 +0530

    updated search header component.

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from "./SearchBox";
+import {RiSettings3Line} from 'react-icons/ri'
+import { TbGridDots } from "react-icons/tb";
 
 const SearchHeader = () => {
   return (
-    <div>SearchHeader</div>
-  )
-}
+    <header className="stikcy top-0 bg-white">
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            src="https://logos-world.net/wp-content/uploads/2020/09/Google-Logo-2015-present.jpg"
+            alt="logo"
+            style={{ width: "auto" }}
+            width={120}
+            height={40}
+          />
+        </Link>
+        <div className='flex-1'>
+          <SearchBox/>
+        </div>
+        <div className='hidden md:inline-flex space-x-2'>
+            <RiSettings3Line className="bg=transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer"/>
+            <TbGridDots className="bg=transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer"/>
+        </div>
+        <button className="ml-2 px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow bg-blue-500 text-sm text-white">Sign in</button>
+      </div>
+    </header>
+  );
+};
 
-export default SearchHeader
+export default SearchHeader;
