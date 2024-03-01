7ae2caebc97e5c0f5bcba126d2fe6617eb26a6e2
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-01 14:42:30 +0530

    image search result component rendoring done.

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import ImageSearchResult from "@/components/ImageSearchResult";
+import WebSearchResults from "@/components/WebSearchResults";
+import Link from "next/link";
+import React from "react";
+const ImageSearchPage = async ({ searchParams }) => {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+  if (!res.ok) throw new Error("Something went wrong");
+  const data = await res.json();
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="sm:text-3xl mb-4">
+          No results found for "{searchParams.searchTerm}"
+        </h1>
+        <p className="sm:text-lg">
+          Try searching the web or image for something else{" "}
+          <Link href={"/"} className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <div>{results && <ImageSearchResult results={data} />}</div>;
+};
 
-const ImageSearchPage = () => {
-  return (
-    <div className='text-red-400'>ImageSearchPage</div>
-  )
-}
-
-export default ImageSearchPage
+export default ImageSearchPage;
