6d6a01a77fe98ea8e69885a73c6bd0e0eedd76e2
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-01 10:44:57 +0530

    search header options component done.

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import SearchBox from "./SearchBox";
 import {RiSettings3Line} from 'react-icons/ri'
 import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 const SearchHeader = () => {
   return (
@@ -27,6 +28,7 @@ const SearchHeader = () => {
         </div>
         <button className="ml-2 px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow bg-blue-500 text-sm text-white">Sign in</button>
       </div>
+      <SearchHeaderOptions/>
     </header>
   );
 };
